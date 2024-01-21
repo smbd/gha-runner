81be53f11c75dc9d83eacd207c71634f5b45f3b6
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2024-01-21 17:25:56 +0900

    ephemeral

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,15 +26,23 @@ echo ${GH_PAT} | grep -q "^ghp_" || abort "\$2(${GH_PAT}) is not valid Github pr
 
 cd /home/runner || abort "chdir failed"
 
-# restore config after second time
-if [ $(find relm -type f -size +1c -name 'dot.*' | wc -l) -eq 3 ] ; then
-  for file in runner credentials credentials_rsaparams ; do
-    ln -s relm/dot.${file} .${file}
-  done
+./config.sh --unattended --ephemeral --name arm64-runner --url ${URL} --pat ${GH_PAT} | grep -q "A runner exists with the same name"
+pipestatus=("${PIPESTATUS[@]}")
+if [ ${pipestatus[0]} -ne 0 ] ; then
+  if [ ${pipestatus[1]} -eq 0 ] ; then
+    # restore relms
+    if [ $(find relm -type f -size +1c -name 'dot.*' | wc -l) -eq 3 ] ; then
+      for file in runner credentials credentials_rsaparams ; do
+        rm -f .${file}
+        ln -s relm/dot.${file} .${file} || abort "missind relm file: ${file}"
+      done
+    else
+      abort "missing relm files, delete self-hosted runner from github.com WebUI"
+    fi
+  else
+    abort "an error is occured, in config.sh"
+  fi
 else
-  # 1st time
-  ./config.sh --unattended --name arm64-runner --url ${URL} --pat ${GH_PAT} || abort "config failed"
-
   # save configs for next time
   for file in runner credentials credentials_rsaparams ; do
     cp .${file} relm/dot.${file}
